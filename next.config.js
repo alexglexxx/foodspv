934a1009fbec84029503ee2cc6081b0bae6d9780
alexglexxx <alexglex@gmail.com>
2026-05-06 00:59:24 +0000

    fix: isolate next build from functions

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // IMPORTANTE: Esto optimiza el build para entornos de servidor como Cloud Functions
-  output: 'standalone', 
-  // Permitimos que Firebase maneje la compresión
-  compress: false,
-  experimental: {
-    // Esto ayuda a que las funciones encuentren las dependencias correctamente
-    externalDir: true,
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
