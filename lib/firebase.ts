133c566f3e9708ac690c8c9cc7e1f5f9505fa6b2
alexglexxx <alexglex@gmail.com>
2026-05-08 00:03:42 +0000

    fix: export firebase auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 import { getFirestore } from "firebase/firestore";
- 
-// Your web app's Firebase configuration
+
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD0ys3AQvcwStU_WX_XhofMhSmOhCvwV_k",
   authDomain: "foodspv-14829.firebaseapp.com",
   projectId: "foodspv-14829",
   storageBucket: "foodspv-14829.firebasestorage.app",
   messagingSenderId: "44869038768",
-  appId: "1:44869038768:web:c0dc9b1da1b20a48d68175"
+  appId: "1:44869038768:web:c0dc9b1da1b20a48d68175",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
